e6927d58c78932150579d17ec7216aa96b498be4
Amr Maket <amrmaket89@gmail.com>
2023-07-21 23:59:12 +0300

    fixed script.js

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -2,14 +2,28 @@ const pages = {}
 
 pages.base_url = "Add Base URL here";
 
-let signinData = {
-    email: "mhmd",
-    password: "fa"
-}
-
 // Common Functions
 pages.print_message = (message) =>{
     console.log(message);
+}
+
+pages.getAPI = async (url) =>{
+    try{
+        return await axios(url)
+    }catch(error){
+        pages.print_message("Error from GET API: " + error)
+    }
+}
+
+pages.postAPI = async (api_url, api_data) => {
+    try{
+        return await axios.post(
+            api_url,
+            api_data
+        );
+    }catch(error){
+        pages.print_message("Error from Linking (POST)" + error)
+    }
 }
 
 pages.loadFor = (page) => {
