65f310d8f786062155d1925648a1c6268468aaa2
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-07-22 02:11:26 +0300

    Adjusted signup js code

---
+++ back-end/connection.php
@@ -0,0 +1,12 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Max-Age: 3600');
+    
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = null;
+    $db_name = "classroomclone_db";
+
+    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
