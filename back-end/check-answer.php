0649250612556d2cfab30aacafca855c542fded7
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-22 01:21:03 +0300

    update checking answer in case forget password

--- back-end/check-answer.php
+++ back-end/check-answer.php
@@ -1,25 +1,24 @@
 <?php
 include('connection.php');
-$user_id=$_POST['user_id'];
+
 $answer=$_POST['answer'];
 $email=$_POST['email'];
 
-$query=$mysqli->prepare('select user_id, first_name,last_name,email,answer
+$query=$mysqli->prepare('select first_name,last_name,email,answer
  from users 
- where user_id=? and answer=? and email=?');
- $query->bind_param('iss',$user_id,$answer,$email);
+ where answer=? and email=?');
+ $query->bind_param('ss',$answer,$email);
  $query->execute();
  $query->store_result();
- $query->bind_result($user_id,$first_name,$last_name,$email,$answer);
+ $query->bind_result($first_name,$last_name,$email,$answer);
  $query->fetch();
  $num_rows=$query->num_rows();
  if($num_rows==0){
-    $response['answer_check']="Your answer is not true!";
+    $response['answer_check']="Wrong answer";
  }
  else{
-    $response['answer_check']="Your answer is right!";
-    $response['right_answer']=$answer;
-    $response['email']=$email;
+    $response['answer_check']="Right answer";
+    
  }
 echo json_encode($response);
 
