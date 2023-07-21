d34de578ddf483241faad04fde80165fcb2b7469
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-22 01:30:12 +0300

    update changing password of a user

--- back-end/change-password.php
+++ back-end/change-password.php
@@ -1,25 +1,24 @@
 <?php
 include('connection.php');
-$user_id=$_POST['user_id'];
 $email=$_POST['email'];
 $new_password=$_POST['new_password'];
-$check_user=$mysqli->prepare('select user_id from users where user_id=? and email=?');
-$check_user->bind_param('is',$user_id,$email);
+$check_user=$mysqli->prepare('select user_id from users where email=?');
+$check_user->bind_param('s',$email);
 $check_user->execute();
 $check_user->store_result();
 $username_exists = $check_user->num_rows();
 if($username_exists==1){
     $hashed_new_password=password_hash($new_password,PASSWORD_BCRYPT);
-    $query=$mysqli->prepare('update users set password=? where user_id=? and email=?');
-    $query->bind_param('sis',$hashed_new_password,$user_id,$email);
+    $query=$mysqli->prepare('update users set password=? where email=?');
+    $query->bind_param('ss',$hashed_new_password,$email);
     $query->execute();
 
-    $response['status']="succes";
-    $response['message']="the password was updated successfully!";
+    $response['status']="Success";
+    $response['message']="Password was updated";
     
 }
 else{
-    $response['status']="failure"; 
+    $response['status']="Failure"; 
 }
 echo json_encode($response);
 ?>
