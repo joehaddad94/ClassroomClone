33b67a90ef721e17bda038ad8abab75f5cb91bb0
Yusuf <yusufshatila457@gmail.com>
2023-07-22 14:05:37 +0300

    Added question and edited input ids

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -27,55 +27,9 @@ pages.loadFor = (page) => {
     eval("pages.page_" + page + "();")
 }
 
-//html Pages
 
-pages.page_signup = () => {
-    const createActBtn = document.getElementById("create-account-btn")
-    console.log(createActBtn)
-    createActBtn.addEventListener("click", () =>{
-        window.location.href = "signup.html"
-    })
+  
 
-    
-    signup.addEventListener("click", async() => {
-        const first_name = document
-            .getElementById("")
-            .value;
-        const last_name = document
-            .getElementById("")
-            .value;
-        const email = document
-            .getElementById("")
-            .value;
-        const password = document
-            .getElementById("")
-            .value;
-        const question = document
-            .getElementById("")
-            .value;
-        const answer = document
-            .getElementById("")
-            .value;
-
-        try {
-            const data = new FormData();
-            data.append("first_name", first_name)
-            data.append("last_name", last_name)
-            data.append("email", email)
-            data.append("password", password)
-            data.append("question", password)
-            data.append("answer", password)
-
-            const index_url = pages.base_url + ""
-            const response = await pages.postAPI(index_url, data)
-            console.log(response)
-            //   fetch("http://localhost/Login-Register/signup.php", {     method: "POST",
-            // body: data   })
-        } catch (error) {
-            console.log(error)
-        }
-    })
-}
 
 pages.page_index = () => {
 
