803c2fe1c2023960dfa9cb6fd278864e02cef325
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-23 22:58:07 +0300

    getting students enrolled in a certain class

--- back-end/get-user-teacher-ofclass.php
+++ back-end/get-user-teacher-ofclass.php
@@ -9,7 +9,7 @@ if(isset($_POST['user_id']) && $_POST['class_id']!=""){
     FROM users
     JOIN roles ON users.role_id = roles.role_id
     JOIN classes ON classes.user_id = users.user_id
-    WHERE classes.class_id = ? AND users.user_id = ?');
+    WHERE classes.class_id = ? AND users.user_id = ? and roles.role_id=2');
     $query->bind_param('ii', $class_id,$user_id);
     $query->execute();
     $result = $query->get_result();
