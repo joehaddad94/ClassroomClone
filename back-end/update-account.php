e07badd58182958949ef156ac554daa80c4e58fb
Amr Maket <amrmaket89@gmail.com>
2023-07-23 23:50:46 +0300

    added update-account.php

--- back-end/update-account.php
+++ back-end/update-account.php
@@ -4,19 +4,17 @@ include('connection.php');
 
 
 if(isset($_POST["new_first_name"]) && $_POST["new_last_name"] != ""){
-    $new_first_namex=$_POST['new_first_name'];
-    $new_last_namex=$_POST['new_last_name'];
-    $response["success"] = "you made it ". $new_first_namex." !";
-    $response["message"]="successfully connected!";
+    $new_first_name=$_POST['new_first_name'];
+    $new_last_name=$_POST['new_last_name'];
 
-    $query = "UPDATE users SET first_name = ?, last_name = ?";
+    $query = "update users set first_name = ?, last_name = ?";
     
     $response = $mysqli->prepare($query);
     $response->bind_param("ssi", $new_first_namex , $new_last_namex);
+
     if ($response->execute()) {
         echo "Profile updated successfully!";
     } else {
         echo "Error updating profile: " . $response->error;
     }
-
-
+}
