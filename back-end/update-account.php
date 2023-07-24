089a7fabab2c6c84a87d149e2212441fff9f375f
Amr Maket <amrmaket89@gmail.com>
2023-07-24 16:24:21 +0300

    fixed upadate-account

--- back-end/update-account.php
+++ back-end/update-account.php
@@ -21,18 +21,4 @@ $user_data = $result->fetch_assoc();
 
 $mysqli->close();
 
-if(isset($_POST["new_first_name"]) && $_POST["new_last_name"] != ""){
-    $new_first_name=$_POST['new_first_name'];
-    $new_last_name=$_POST['new_last_name'];
-
-    $query = "update users set first_name = ?, last_name = ?";
-    
-    $response = $mysqli->prepare($query);
-    $response->bind_param("ssi", $new_first_namex , $new_last_namex);
-
-    if ($response->execute()) {
-        echo "Profile updated successfully!";
-    } else {
-        echo "Error updating profile: " . $response->error;
-    }
-}
+echo json_encode($user_data);
