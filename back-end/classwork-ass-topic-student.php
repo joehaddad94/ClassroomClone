0d219020ca09e1cd9768a38ab76f4ead959a381c
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-24 23:04:36 +0300

    getting assignments grouped by topics per class

--- back-end/classwork-ass-topic-student.php
+++ back-end/classwork-ass-topic-student.php
@@ -1,15 +1,9 @@
 <?php
 include('connection.php');
-// echo "connected successfully!";
 $class_id = $_POST['class_id'];
 
 if ($_POST['class_id'] != "") {
-    $query = $mysqli->prepare('SELECT topics.topic_name,assignments.title,assignments.instructions,assignments.due_date 
-    FROM topics    
-    JOIN classes ON classes.class_id = topics.class_id 
-    JOIN assignments ON assignments.topic_id = topics.topic_id 
-    WHERE topics.class_id = ?
-    GROUP BY topics.topic_name');
+    $query = $mysqli->prepare('SELECT topics.topic_name, assignments.title, assignments.instructions, assignments.due_date FROM topics JOIN classes ON classes.class_id = topics.class_id JOIN assignments ON assignments.topic_id = topics.topic_id WHERE topics.class_id = ? GROUP BY topics.topic_name, assignments.title');
     $query->bind_param('i', $class_id);
     $query->execute();
     $result = $query->get_result();
@@ -17,9 +11,16 @@ if ($_POST['class_id'] != "") {
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            $response[] = $row;            
+            $topic_name = $row['topic_name'];
+            if (!isset($response[$topic_name])) {
+                $response[$topic_name] = array();
+            }
+            $response[$topic_name][] = array(
+                'title' => $row['title'],
+                'instructions' => $row['instructions'],
+                'due_date' => $row['due_date']
+            );
         }
-     
     } else {
         $response['message'] = 'not found';
     }
@@ -28,3 +29,4 @@ if ($_POST['class_id'] != "") {
 }
 
 echo json_encode($response);
+?>
