2631b4cde71c6a133099236f888a4fd257a507a4
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-25 13:25:49 +0300

    created the assignment page

--- back-end/create-assignment.php
+++ back-end/create-assignment.php
@@ -9,6 +9,8 @@ $due_date = $_POST['due_date'];
 $class_id = $_POST['class_id'];
 $user_id = $_POST['user_id'];
 
+$instructions = isset($_POST['instructions']) ? $_POST['instructions'] : NULL;
+
 // Check if attachment_id is set, otherwise set to NULL
 $attachment_id = isset($_POST['attachment_id']) ? $_POST['attachment_id'] : NULL;
 
